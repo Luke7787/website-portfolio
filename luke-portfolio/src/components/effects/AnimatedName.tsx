07756f2374b5b4389be7190cae1d732de0f6705d
Luke Zhuang <Lukezhuang@yahoo.com>
2026-02-01 17:38:05 -0800

    got the text animation down

--- luke-portfolio/src/components/effects/AnimatedName.tsx
+++ luke-portfolio/src/components/effects/AnimatedName.tsx
@@ -9,11 +9,11 @@ type AnimatedNameProps = {
 export default function AnimatedName({ children }: AnimatedNameProps) {
   return (
     <div className="relative inline-block overflow-hidden">
-      {/* Blue sliding background */}
-      <div className="name-reveal-bg absolute inset-0 z-10 bg-[#1E90FF]" />
+      {/* Text (below animation) */}
+      <span className="relative z-10">{children}</span>
 
-      {/* Content comes from parent */}
-      <span className="relative z-20">{children}</span>
+      {/* Blue sliding background (above text) */}
+      <div className="name-reveal-bg absolute inset-0 z-20 bg-[#1E90FF]" />
     </div>
   );
 }
