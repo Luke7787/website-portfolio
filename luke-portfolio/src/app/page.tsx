360d7c08eed3d8d6606df45fb9a2729fffc92047
Luke Zhuang <Lukezhuang@yahoo.com>
2026-02-09 12:58:11 -0800

    updated style to h2 text

--- luke-portfolio/src/app/page.tsx
+++ luke-portfolio/src/app/page.tsx
@@ -95,7 +95,15 @@ export default function Page() {
               ABOUT
             </p>
 
-            <h2 className="mt-2 text-4xl font-bold tracking-[0.15em] text-white/90">
+            <h2
+              className="
+                mt-2
+                text-[2rem]
+                font-normal
+                tracking-[0.05em]
+                text-white
+              "
+            >
               {"Hi! I'm Luke Zhuang"}
             </h2>
           </div>
